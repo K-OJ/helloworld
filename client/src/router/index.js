5b4e0fd0b3f5e8091e34d17435a6481ea4591118
kojin <kojin777@ubcare.co.kr>
2026-03-13 12:38:09 +0900

    feat: GitHub Pages 자동 배포 워크플로우 추가

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import MainView from '../views/MainView.vue'
 import IntroView from '../views/IntroView.vue'
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
